5c6020034c579bf2e1a2d00b6c46fd4096ca6bfa
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2023-08-21 22:19:23 +0530

    react vite and core react project created

---
+++ 01vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai.jsx"
+
+function App() {
+  const username = "Vishal";
+  return (
+    <>
+      <Chai/>
+      <h1>Chai aur react with vite | {username}</h1>
+    </>
+  )
+}
+
+export default App
