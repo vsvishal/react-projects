270462e1969537742786dc36225b6a4619c6cd96
Sharma <vishal-rameshchandra.sharma@capgemini.com>
2023-08-25 08:51:23 +0530

    03tailwindprops project added

--- basic-react/src/index.js
+++ basic-react/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const heading = React.createElement(
+  "h1",
+  { style: { backgroundColor: "#212121" } },
+  "Hello from Vishal"
 );
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
