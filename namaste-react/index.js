e396c54375180e49102079b67024d23c21d646ea
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2023-08-28 12:07:42 +0530

    Notes updated

--- namaste-react/index.js
+++ namaste-react/index.js
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", style: { backgroundColor: "green" } },
-  "Hello world from React"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", style: { backgroundColor: "green" } },
+//   "Hello world from React"
+// );
+
+// React Element
+const title = <h2>I am title React element</h2>;
+
+// React Component
+const TitleComponent = () => {
+  return <h3>I am Titile Component</h3>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <>
+      <h1 className="head">This is Heading Component</h1>
+      {title}
+      <TitleComponent />
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
 
 // const root = document.getElementById("root");
 
